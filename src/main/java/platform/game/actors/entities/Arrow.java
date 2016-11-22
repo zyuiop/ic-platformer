6fa2a61359f45e9c8b22c0a800f706212ec78580
zyuiop <zyuiop@zyuiop.net>
2016-11-22 12:30:20 +0100

    Remove debug

--- src/main/java/platform/game/actors/entities/Arrow.java
+++ src/main/java/platform/game/actors/entities/Arrow.java
@@ -26,25 +26,7 @@ public class Arrow extends AtachableProjectible {
 
 	@Override
 	protected void hitBlock(Actor solidActor, Vector delta) {
-		/*Vector center = solidActor.getPosition();
-		// angle :
-		double angle = getVelocity().getAngle();
-		double cos = Math.abs(Math.cos(angle));
-		double sin = Math.abs(Math.sin(angle));
-
-		// center + (cos(angle), sin(angle))*magnitude
-		// magnitude :
-		double magnitude = getPosition().distance(solidActor.getPosition());
-
-		Vector diff = center.add(new Vector(cos, sin).mul(magnitude));
-		// Vector diff = Vector.Y.rotated(getVelocity().getAngle());
-		// diff = diff.add(getPosition().sub(solidActor.getPosition()));
-
-		System.out.println(getPosition() + " : " + solidActor.getPosition() + " ==> " + diff + ", " + angle + ", " + cos + ", " + sin + ", " + magnitude);
-		*/
-
 		Vector diff = getPosition().sub(solidActor.getPosition());
-		System.out.println(solidActor.getPosition() + " " + getPosition() + " => " + diff);
 		attachTo(solidActor, diff, getVelocity().getAngle());
 	}
 }
