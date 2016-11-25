5f3dd486c8c0604d1eae54d8cf7e4fe2800b5c7f
zyuiop <zyuiop@zyuiop.net>
2016-11-25 23:09:50 +0100

    grébl

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@
 
 targetName=Platformer-$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT
 
+echo "Deploying to "$targetName
+
 scp target/icplatformer-0.0.1-SNAPSHOT.jar deploy@ks.zyuiop.net:/srv/archive.zyuiop.net/$targetName
 ssh deploy@ks.zyuiop.net cp /srv/archive.zyuiop.net/$targetName /srv/archive.zyuiop.net/Platformer-LATEST.jar
