879b9ef48c475f30252faefc32816ac5e20336cf
zyuiop <zyuiop@zyuiop.net>
2016-11-25 22:59:29 +0100

    Better ?

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,5 @@
 
 targetName=Platformer-$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT
 
-mkdir ~/.ssh/
-echo $SSH_PRIVATE > ~ /.ssh/id_rsa
-echo $SSH_PUBLIC > ~ /.ssh/id_rsa.pub
-
-
 scp target/icplatformer-0.0.1-SNAPSHOT.jar deploy@ks.zyuiop.net:/srv/archive.zyuiop.net/$targetName
 ssh deploy@ks.zyuiop.net cp /srv/archive.zyuiop.net/$targetName /srv/archive.zyuiop.net/Platformer-LATEST.jar
