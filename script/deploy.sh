e120405778f8d1e635e757a55c29ad8011f51389
zyuiop <zyuiop@zyuiop.net>
2016-11-25 23:22:59 +0100

    Fix deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,6 @@
-targetName=Platformer-$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT
+targetName=Platformer-$TRAVIS_BUILD_NUMBER-${TRAVIS_COMMIT:0:8}.jar
 
 echo "Deploying to "$targetName
 
-scp -oStrictHostKeyChecking=no target/icplatformer-0.0.1-SNAPSHOT.jar deploy@ks.zyuiop.net:/srv/archive.zyuiop.net/$targetName
-ssh -oStrictHostKeyChecking=no deploy@ks.zyuiop.net cp /srv/archive.zyuiop.net/$targetName /srv/archive.zyuiop.net/Platformer-LATEST.jar
+scp -oStrictHostKeyChecking=no target/icplatformer-0.0.1-SNAPSHOT.jar deploy@ks.zyuiop.net:/srv/archive.zyuiop.net/Platformer/$targetName
+ssh -oStrictHostKeyChecking=no deploy@ks.zyuiop.net cp /srv/archive.zyuiop.net/Platformer/$targetName /srv/archive.zyuiop.net/Platformer/Platformer-LATEST.jar
