fe6bfa7223fc01d5c34c054c425f5047ac2ced76
zyuiop <zyuiop@zyuiop.net>
2016-11-25 23:17:52 +0100

    Fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,5 @@ targetName=Platformer-$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT
 
 echo "Deploying to "$targetName
 
-scp target/icplatformer-0.0.1-SNAPSHOT.jar deploy@ks.zyuiop.net:/srv/archive.zyuiop.net/$targetName
-ssh deploy@ks.zyuiop.net cp /srv/archive.zyuiop.net/$targetName /srv/archive.zyuiop.net/Platformer-LATEST.jar
+scp -oStrictHostKeyChecking=no target/icplatformer-0.0.1-SNAPSHOT.jar deploy@ks.zyuiop.net:/srv/archive.zyuiop.net/$targetName
+ssh -oStrictHostKeyChecking=no deploy@ks.zyuiop.net cp /srv/archive.zyuiop.net/$targetName /srv/archive.zyuiop.net/Platformer-LATEST.jar
