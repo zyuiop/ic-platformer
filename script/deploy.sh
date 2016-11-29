773d0b1340a48fc99a83e3143bcf5fa75e852198
zyuiop <zyuiop@zyuiop.net>
2016-11-29 11:22:39 +0100

    New deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,7 +3,15 @@
 # As this project is still private, we obfuscate it strongly first
 java -jar script/proguard.jar @script/config.pro
 
+echo "No changes." > CHANGES.txt
+
+if [ -n "$TRAVIS_COMMIT_RANGE" ]
+	then echo ${git log $TRAVIS_COMMIT_RANGE} > CHANGES.txt
+	cat CHANGES.txt
+fi
+
 # Then we deploy it using SSH because I still don't want to learn how to use rsync o/
 targetName=Platformer-$TRAVIS_BUILD_NUMBER-${TRAVIS_COMMIT:0:8}.jar
 scp -oStrictHostKeyChecking=no platformer.jar deploy@ks.zyuiop.net:/srv/archive.zyuiop.net/Platformer/$targetName
+scp -oStrictHostKeyChecking=no CHANGES.txt deploy@ks.zyuiop.net:/srv/archive.zyuiop.net/Platformer/
 ssh -oStrictHostKeyChecking=no deploy@ks.zyuiop.net cp /srv/archive.zyuiop.net/Platformer/$targetName /srv/archive.zyuiop.net/Platformer/Platformer-LATEST.jar
