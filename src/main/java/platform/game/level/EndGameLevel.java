15119ec293c52665a40bd7cb941316786b2bcd01
zyuiop <zyuiop@zyuiop.net>
2016-12-10 19:41:01 +0100

    Resize

--- src/main/java/platform/game/level/EndGameLevel.java
+++ src/main/java/platform/game/level/EndGameLevel.java
@@ -1,10 +1,13 @@
 package platform.game.level;
 
 
+import platform.game.KeyBindings;
+import platform.game.KeyBindings.Key;
 import platform.game.World;
 import platform.game.actors.environment.Background;
 import platform.game.actors.ui.ButtonActor;
 import platform.game.actors.ui.TextBox;
+import platform.game.menus.main.MainMenuLevel;
 import platform.game.menus.settings.KeyBindingsLevel;
 import platform.util.Input;
 import platform.util.Output;
@@ -18,6 +21,10 @@ import java.awt.*;
 public class EndGameLevel extends Level {
 	@Override
 	public void update(Input input) {
+		if (KeyBindings.getInstance().isDown(input, Key.MENU)) {
+			getWorld().setNextLevel(new MainMenuLevel());
+			getWorld().nextLevel();
+		}
 		// sobre svp
 	}
 
@@ -31,10 +38,10 @@ public class EndGameLevel extends Level {
 		super.register(world);
 
 		try {
-			Font font = new Font(Font.MONOSPACED, Font.BOLD, 30);
+			Font font = new Font(Font.SANS_SERIF, Font.BOLD, 30);
 
-			world.register(new TextBox(new Vector(0, 0), null, font, Color.BLACK, 0, 1, 2.5, 0, "Bravo !"));
-			world.register(new TextBox(new Vector(0, -1), null, font, Color.BLACK, 0, 1, 5, 0, "Vous avez terminé le jeu"));
+			world.register(new TextBox(new Vector(0, 0.5), null, font, Color.BLACK, 0, 1, 2.5, 0, "Bravo !"));
+			world.register(new TextBox(new Vector(0, -0.5), null, font, Color.BLACK, 0, 1, 5, 0, "Vous avez terminé le jeu"));
 
 			world.register(new Background("background.hills", true, false));
 		} catch (Exception e) {
